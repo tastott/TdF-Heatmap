1dbfd3c191019a8abbbd2641cf300071875ef7e5
Tim Stott <ta.stott@googlemail.com>
2014-06-26 23:11:24 +0100

    Get stage count for each town

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,69 @@
 $(document).ready(function(){
 	
-	getTdfTowns('http://www.letour.fr/HISTO/us/TDF/villes.html', function(towns){
-		alert(towns.length);
-	})
+	getTdfTowns('http://www.letour.fr', 'HISTO/us/TDF/villes.html', function(towns){
+		
+		//towns = towns.slice(0, 100);
+
+		var i = 0;
+
+		towns.forEach(function(town){
+			getTownStages(town.url, function(stages){
+				town.StageCount = stages.length;
+				if(++i == towns.length){
+					
+					
+					
+				}
+			});
+		});
+
+		
+	});
 
 
 });
 
-function getTdfTowns(url, callback){
+function getHtml(url, callback){
 	$.ajax({
 	    url: url,
 	    type: 'GET',
 	    success: function(response) {
-	        var towns = $(response.responseText)
-				        	.find('li.ville > a')
-				        	.map(function(i, a){
-				        		return {
-				        			name: a.text,
-				        			url: a.href
-				        		}		
-				        	})
-				        	.toArray();
-
-        	callback(towns);
+	        callback(response.responseText);
 	    }
 	});
+}
+
+function getTdfTowns(domain, url, callback){
+	console.log('Getting towns from ' + url + '...');
+	getHtml(domain + '/' + url, function(html){
+		var towns = $(html)
+			        	.find('li.ville > a')
+			        	.map(function(i, a){
+			        		return {
+			        			name: a.text,
+			        			url: domain + a.href.substring(7, a.href.length)
+			        		}		
+			        	})
+			        	.toArray();
+		console.log('Got towns')	        	
+    	callback(towns);
+	});
+}
+
+function getTownStages(url, callback){
+	console.log('Getting stages for town ' + url + '...');
+	getHtml(url, function(html){
+		var stages = $(html).find('table.liste')
+							.find('tr')
+							.map(function(i, tr){
+								return {
+									index: i
+								};
+							})
+							.toArray();
+		stages = stages.slice(1, stages.length);
+
+		console.log('Got stages for town ' + url);
+		callback(stages);
+	})
 }
