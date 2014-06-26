6fa9e7b83c01d37b46b33573d1d25b98ea95e796
Tim Stott <ta.stott@googlemail.com>
2014-06-26 08:03:36 +0100

    getTdfTowns()

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,28 @@
 $(document).ready(function(){
-	alert('blah');
+	
+	getTdfTowns('http://www.letour.fr/HISTO/us/TDF/villes.html', function(towns){
+		alert(towns.length);
+	})
+
+
 });
+
+function getTdfTowns(url, callback){
+	$.ajax({
+	    url: url,
+	    type: 'GET',
+	    success: function(response) {
+	        var towns = $(response.responseText)
+				        	.find('li.ville > a')
+				        	.map(function(i, a){
+				        		return {
+				        			name: a.text,
+				        			url: a.href
+				        		}		
+				        	})
+				        	.toArray();
+
+        	callback(towns);
+	    }
+	});
+}
