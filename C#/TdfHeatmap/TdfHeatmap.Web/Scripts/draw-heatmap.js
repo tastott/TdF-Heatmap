52a613b464d85440adf173dcd3b172601577dec6
Tim Stott <tstott@oxfordeconomics.com>
2014-07-16 09:01:41 +0100

    Fixed heatmap

--- C#/TdfHeatmap/TdfHeatmap.Web/Scripts/draw-heatmap.js
+++ C#/TdfHeatmap/TdfHeatmap.Web/Scripts/draw-heatmap.js
@@ -1,4 +1,4 @@
-﻿define(['jquery', 'heatmap-gmaps', 'async!http://maps.google.com/maps/api/js?sensor=false'], function ($, HeatmapOverlay) {
+﻿define(['jquery', 'heatmap-gmaps', 'async!google-maps'], function ($, HeatmapOverlay) {
 
     return {
         draw: function (data) {
@@ -28,6 +28,19 @@
             // this is important, because if you set the data set too early, the latlng/pixel projection doesn't work
             google.maps.event.addListenerOnce(map, "idle", function () {
                 heatmap.setDataSet(data);
+
+                //Add marker for each town
+                data.filter(function (town, index) { return index < 10; })
+                    .forEach(function (town) {
+                        var latlng = new google.maps.LatLng(town.Location.Latitude, town.Location.Longitude);
+
+                        // To add the marker to the map, use the 'map' property
+                        var marker = new google.maps.Marker({
+                            position: latlng,
+                            map: map,
+                            title: town.Name
+                        });
+                });
             });
         }
     }
